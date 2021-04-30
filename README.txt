9f3ae6a169650e46e880b4833ead1a460757ec3e
himanshu-learner <himanshumodi@gmail.com>
2021-04-30 13:27:05 +0100

    Cleaned up game with finishing touches Added a treasure image instead of petrol can Greyed out buttons at the restart screen Some other minor refactoring

---
+++ README.txt
@@ -0,0 +1,35 @@
+Monty Hall Problem
+
+https://en.wikipedia.org/wiki/Monty_Hall_problem
+
+Suppose you're on a game show, and you're given the choice of three doors: Behind one door is a car; behind the others, goats. 
+You pick a door, say No. 1. 
+The host, who knows what's behind the doors, opens another door, say No. 3, which has a goat. 
+He then says to you, "Do you want to pick door No. 2? Or do you want to go with door No. 1?" 
+What do you do?
+
+Program:
+The js app simulates the above game:
+1. There are three doors to pick from. One has a treasure, the others have lemons (don't ask why I went with these options. Initially, instead of treasure I had gas/petrol can)
+2. You choose any door you want.
+3. The host will show you one of the remaining doors that has lemons.
+4. Now you have to choose whether you will stay with your initial choice, or switch to the remaining door - which is unopened.
+
+When Marilyn vos Savant, a high IQ autor/lecturer, suggested the player must always switch, it created a fair bit of controversy.
+
+Now you can see if this is indeed the correct choice.
+
+There is also a simulator included so you can see probabilistic values of both choices.
+
+Have fun!
+
+I made this to practice Javascript. If you have any suggestions about the code, would love to hear them.
+
+WHAT I LEARNT:
+1. DOM element selection - especially ignoring and picking clicks and using them as user inputs. A little more complex than taking input values from form fields 
+2. DOM manipulation. Changing colors and values of buttons through classes, images, innterHTML content.
+3. Appreciating the effort requried for clean up - like reseting colors at the start of a game, score content, modal behaviour, etc.
+
+
+
+
--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //global variables
 var totalGames = 0;
-var gameStage = 1;
 var gamesWon = 0;
-var gameDoors;
+var gameDoors; //this will hold the winning door and host selected door.
+var gameStage = 1; //=1, when start of game, where game selects door; =2, when host shows a loosing door and asks gamer for final choice; =3, when game ends 
+
 
 //game start modal
 $('#game-start-modal').on('hidden.bs.modal', function () {
@@ -20,46 +21,48 @@ document.getElementById('simulator-form').addEventListener('submit', function(e)
     let gamesToPlay = e.target.gameCount.value;
     let alwaysStay = e.target.stage2Choice[0].checked;
     for (gameIter=1; gameIter<=gamesToPlay; gameIter++) {
-        gameStage = 1;
-        let inputDoor = Math.floor(Math.random()*3)+1;
+        gameStage = 1; //initiage game stage for every iteration
+        let inputDoor = Math.floor(Math.random()*3)+1; //simulator picks first door at random
         gameStage1(inputDoor);
         if (alwaysStay) {
-            gameResult = determineResult(inputDoor);
+            gameResult = determineResult(inputDoor); //gamer plays with the original choice
             //console.log(`game no. ${gameIter}, Door Selected: ${inputDoor}, Winning Door: ${gameDoors.winningDoor}`);
-            //console.log(`Game Result: ${gameResult}`);
+            //console.log(`Game Result: ${gameResult}`); //keep this logs to audit results
         } else {
-            let switchedDoor = thirdDoorSelector(inputDoor, gameDoors.hostDoor);
+            let switchedDoor = thirdDoorSelector(inputDoor, gameDoors.hostDoor); //gamer switches to the door he has not chosen, and is still unopened
             gameResult = determineResult(switchedDoor);  
             //console.log(`game no. ${gameIter}, Door Selected: ${switchedDoor}, Winning Door: ${gameDoors.winningDoor}`);
             //console.log(`Game Result: ${gameResult}`); 
         }
         
     }
-    resetUi();
-    gameResultConsoleUi('simulation');
-    ScoreboardUi();
+    resetUi(); //reset door buttons and images, in case simulation is run in between a game initiated by the user
+    gameResultConsoleUi('simulation'); //send message to console that a simulation was run
+    ScoreboardUi(); //print the simulation score
     totalGames = 0;
     gamesWon = 0;
     $('#simulation-inputs-modal').modal('hide');
     e.preventDefault();
 });
 
+//game elements if user wants to play
 //register door selection
 document.getElementById('door-div').addEventListener('click', function (e) {
     let clickedElement = e.target.classList;
-    for (var i = 1; i <= 3; i++) {
+    for (var i = 1; i <= 3; i++) { //loop to determine if, and which door was clicked.
         if (clickedElement.contains(`door-${i}`)) {
             let doorSelected = i;
             if (gameStage === 1) {
                 gameStage1(doorSelected); //function to determine the winning door and assign a door to a host
                 initiateStage2UI(doorSelected); // update console with stage 2 instructions
-                console.log(gameDoors);
+                //console.log(gameDoors); //uncomment this if you want to see the winning dor in the console
             } else if (gameStage === 2) {
-                if (!clickedElement.contains('host-selected')) {
+                if (!clickedElement.contains('host-selected')) { //if statement required to ignore clicks on door selected by host
                     gameResult = determineResult(doorSelected, gameDoors);
+                    ScoreboardUi(); //order important. this function makes all the door selector buttons grey, gameResultDoorDivUI indicates win/loss color
                     gameResultDoorDivUi(gameResult, doorSelected);
                     gameResultConsoleUi(gameResult);
-                    ScoreboardUi();
+                    
                 }
             }
         }
@@ -70,8 +73,8 @@ document.getElementById('door-div').addEventListener('click', function (e) {
 document.getElementById('console').addEventListener('click', function (e) {
     if (e.target.id === 'restart') {
         gameStage = 1;
+        ScoreboardUi(); //order important. this function makes the door selector buttons grey, resetUI makes it blue again
         resetUi();
-        ScoreboardUi();
     };
 
 })
@@ -129,7 +132,6 @@ function determineResult(doorSelected) {
     gameStage = 3;
     totalGames += 1;
     if (message === 'win') gamesWon += 1;
-    //winRatio = gamesWon / totalGames;
     return message;
 }
 
@@ -138,6 +140,7 @@ function initiateStage2UI(doorSelected) {
     let doorRemaining = thirdDoorSelector(doorSelected, gameDoors.hostDoor);
     $('#stage2-modal').modal('show');
     const modalBody = document.querySelector('.stage2-message');
+    //modal to give stage 2 instructions
     modalBody.innerHTML = `
     <p>You Selected Door ${doorSelected}</p>
     </p>The host shows you that Door ${gameDoors.hostDoor} has a lemon behind </p>
@@ -145,10 +148,11 @@ function initiateStage2UI(doorSelected) {
     <p>Stay with the door you chose?</p>
     <p>Or switch to Door ${doorRemaining}?</p>
     <p>Choose wisely...</p>`
+    //update door selector buttons with messages, classes and image
     document.querySelector(`input.door-${doorSelected}`).value = `Door ${doorSelected} - Stay with me!`;
     document.querySelector(`input.door-${doorRemaining}`).value = `Door ${doorRemaining} - Switch to me!`;
     document.querySelector(`input.door-${gameDoors.hostDoor}`).value = `Door ${gameDoors.hostDoor} - Lemon Here`;
-    document.querySelector(`input.door-${gameDoors.hostDoor}`).classList.add("btn-secondary", "host-selected");
+    document.querySelector(`input.door-${gameDoors.hostDoor}`).classList.add("btn-secondary", "host-selected"); //host-selected class gets used to ignore clicks on this element in 2nd stage
     document.querySelector(`input.door-${gameDoors.hostDoor}`).classList.remove("btn-primary");
     document.getElementById('console').innerHTML = `
     <h3 class="text-center">Console</h3>
@@ -160,7 +164,7 @@ function initiateStage2UI(doorSelected) {
     //code to insert lemon image in host selected door
     let hostSelectedDoorElement = document.querySelector(`img.door-${gameDoors.hostDoor}`);
     hostSelectedDoorElement.src = `images/lemon-1.jpg`;
-    hostSelectedDoorElement.classList.add('lemon-treasure', 'host-selected');
+    hostSelectedDoorElement.classList.add('lemon-treasure', 'host-selected');//host-selected class gets used to ignore clicks on this element in 2nd stage. lemon-treasure gets used for styling
 }
 
 function gameResultConsoleUi(result) {
@@ -171,12 +175,14 @@ function gameResultConsoleUi(result) {
     } else if (result === 'simulation') {
         message = 'You ran a simulation'
     }
+    //console includes correct message and a restart button
     document.getElementById('console').innerHTML = `
     <h1>${message}</h1>
     <input type="submit" id="restart" value="Restart" class="btn-block btn btn-primary" id="restart"
             data-bs-toggle="modal" data-bs-target="#game-start-modal">`;
 }
 
+//update right messages, image and classes to door selector buttons
 function gameResultDoorDivUi(result, doorSelected) {
     let doorRemaining = thirdDoorSelector(doorSelected, gameDoors.hostDoor);
     if (result === 'win') {
@@ -196,7 +202,7 @@ function gameResultDoorDivUi(result, doorSelected) {
     }
     
     document.querySelector(`input.door-${doorSelected}`).value = doorSelectedMessage;
-    document.querySelector(`input.door-${doorSelected}`).classList.remove('btn-primary');
+    document.querySelector(`input.door-${doorSelected}`).classList.remove('btn-primary', 'btn-secondary');
     document.querySelector(`input.door-${doorSelected}`).classList.add(doorSelectedClass);
     document.querySelector(`input.door-${doorRemaining}`).value = doorRemainingMessage;
     document.querySelector(`input.door-${doorRemaining}`).classList.remove('btn-primary');
@@ -209,7 +215,13 @@ function gameResultDoorDivUi(result, doorSelected) {
 }
 
 function ScoreboardUi() {
-    totalGames === 0 ? winRatio = 0 : winRatio = gamesWon/totalGames;
+    totalGames === 0 ? winRatio = 0 : winRatio = gamesWon/totalGames; //ternary required to ignore NAN when total games is 0 at the start
+    let doorButtons = document.querySelectorAll(`input.door-btn`);
+    doorButtons.forEach(elem => {
+        elem.classList.remove('btn-success', 'btn-danger', 'btn-primary', 'host-selected');
+        elem.classList.add('btn-secondary');
+    });
+
     document.getElementById('score-body').innerHTML = `
     <h3 class="text-center">Score</h3>
     <h5>Total Games Played: ${totalGames}</h5>
